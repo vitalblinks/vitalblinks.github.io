6833f89f29a36f5e3dc894f87b61ac570f34f0a1
fenzhongwu <fxw1803@rit.edu>
2017-09-19 23:20:21 -0500

    Form Testing 2

--- form.php
+++ form.php
@@ -1,17 +1,8 @@
 <?php
 
-if($_POST["submit"]) {
-    $recipient="12345rockbell@gmail.com";
-    $subject="Form Submission for Vitalblinks";
-    $sender=$_POST["sender"];
-    $senderEmail=$_POST["fullname"];
-    $message=$_POST["phone"];
+	$to = "12345rockbell@gmail.com"
+	$header ="testing"
+	mail($to,$email_subject,$email_body,$headers);
 
-    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
-
-    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
-
-    $thankYou="<p>Thank you! Your message has been sent.</p>";
-}
 
 ?>
